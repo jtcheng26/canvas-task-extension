067355b445644dea98a9699834b3a0af666e83f1
jtcheng26 <jtcheng226@gmail.com>
2020-11-29 21:03:16 -0500

    Fix locality issue with tests

--- src/pages/Content/modules/components/Task.unit.test.jsx
+++ src/pages/Content/modules/components/Task.unit.test.jsx
@@ -7,10 +7,19 @@ const color = 'rgb(0, 0, 0)',
   name = 'Dummy Assignment',
   points_possible = 10;
 let due_at = new Date();
-due_at.setUTCMonth(1);
-due_at.setUTCDate(1);
-due_at.setUTCFullYear(1990);
-due_at.setUTCHours(10, 10, 10, 10);
+due_at.setMonth(1);
+due_at.setDate(1);
+due_at.setFullYear(1990);
+due_at.setHours(10, 10, 10, 10);
+const due_date = due_at.toLocaleString('en-US', {
+  month: 'short',
+  day: 'numeric',
+});
+const due_time = due_at.toLocaleString('en-US', {
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true,
+});
 due_at = due_at.toISOString();
 const mockData = {
   color,
@@ -37,7 +46,7 @@ describe('<Task />', () => {
   it('renders points and due date', () => {
     let wrapper = shallow(<Task assignment={mockData} />);
     expect(wrapper.find('TaskBottom').text()).toBe(
-      points_possible + ' points \xa0|\xa0 ' + 'Mar 1 at 5:10 AM'
+      points_possible + ` points \xa0|\xa0 ${due_date} at ${due_time}`
     );
   });
 
