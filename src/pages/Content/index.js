0ea18b22d0501f305b7e925bed464509beba7f66
jtcheng26 <jtcheng226@gmail.com>
2020-12-28 21:21:13 -0500

    Use mutation observers to detect container

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,3 +1,25 @@
-import run from './modules/init';
+import runApp from './modules/init';
 
-run();
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    for (let addedNode of mutation.addedNodes) {
+      if (
+        'classList' in addedNode &&
+        addedNode.classList.contains('Sidebar__TodoListContainer')
+      )
+        createSidebar(addedNode);
+    }
+  });
+});
+
+const containerList = document.getElementsByClassName(
+  'Sidebar__TodoListContainer'
+);
+if (containerList.length > 0) createSidebar(containerList[0]);
+else
+  observer.observe(document.getElementById('right-side'), { childList: true });
+
+function createSidebar(container) {
+  observer.disconnect();
+  runApp(container);
+}
--- src/pages/Content/modules/init.jsx
+++ src/pages/Content/modules/init.jsx
@@ -2,35 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-export default function run() {
-  let counter = 0;
-
-  function findContainer() {
-    counter++;
-    let container = document.getElementsByClassName(
-      'Sidebar__TodoListContainer'
-    );
-    if (container.length > 0) {
-      clearInterval(interval);
-      container = container[0];
-      while (container.firstChild) container.removeChild(container.lastChild);
-      console.log(
-        'Check out the repo! https://github.com/jtcheng26/canvas-task-extension'
-      );
-      createSidebar(container);
-    } else if (counter >= 300) {
-      clearInterval(interval);
-      console.log('Container not found');
-    }
-  }
-
-  function createSidebar(container) {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      container
-    );
-  }
-  const interval = setInterval(findContainer, 10);
+export default function runApp(container) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    container
+  );
+  console.log(
+    'Check out the repo! https://github.com/jtcheng26/canvas-task-extension'
+  );
 }
