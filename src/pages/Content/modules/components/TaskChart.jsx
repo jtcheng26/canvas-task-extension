53fae9cbbc9538d3205ecb1b02769af10a7a779c
jtcheng26 <jtcheng226@gmail.com>
2021-01-05 21:28:31 -0500

    0-point assignments no longer show up in progress chart

--- src/pages/Content/modules/components/TaskChart.jsx
+++ src/pages/Content/modules/components/TaskChart.jsx
@@ -24,7 +24,10 @@ export default function TaskChart({ courses, assignments, setCourse }) {
       name: course.name,
       idx: course.position,
     };
-  });
+  });/*
+  assignments = assignments.filter((assignment) => {
+    return assignment.points_possible > 0; // don't include 0-point assignments in chart
+  });*/
   assignments.forEach((assignment) => {
     classes[assignment.course_id].total++;
     if (assignment.user_submitted || assignment.grade > 0)
