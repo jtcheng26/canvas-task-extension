b7ea8dce08eb9c8124d6c5b19810d8f3beb154c3
jtcheng26 <jtcheng226@gmail.com>
2021-02-19 13:38:32 -0500

    Update manifest version to v3 and removed eval from webpack devtool

--- webpack.config.js
+++ webpack.config.js
@@ -152,7 +152,7 @@ var options = {
 };
 
 if (env.NODE_ENV === 'development') {
-  options.devtool = 'eval-cheap-module-source-map';
+  options.devtool = 'cheap-module-source-map';
 } else {
   options.optimization = {
     minimize: true,
