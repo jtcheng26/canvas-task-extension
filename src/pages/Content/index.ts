e8edb1d0d73793c92d0aaf97c3349bc5197c71f4
jtcheng26 <jtcheng226@gmail.com>
2021-09-03 19:36:08 -0400

    Update manifest and index for firefox compatibility

--- src/pages/Content/index.ts
+++ src/pages/Content/index.ts
@@ -4,67 +4,89 @@ import { Options } from './modules/types';
 
 const rightSide = document.getElementById('right-side');
 
+const storedUserOptions = [
+  'startDate',
+  'period',
+  'startHour',
+  'startMinutes',
+  'sidebar',
+  'dash_courses',
+];
+
+function optionsOrDefaults(result: Options): Options {
+  return {
+    startDate: !result.startDate ? 1 : result.startDate,
+    startHour: !result.startHour ? 15 : result.startHour,
+    startMinutes: !result.startMinutes ? 0 : result.startMinutes,
+    period: !(
+      result.period === 'Day' ||
+      result.period === 'Week' ||
+      result.period === 'Month'
+    )
+      ? 'Week'
+      : result.period,
+    sidebar:
+      result.sidebar !== false && result.sidebar !== true
+        ? false
+        : result.sidebar,
+    dash_courses:
+      result.dash_courses !== false && result.dash_courses !== true
+        ? false
+        : result.dash_courses,
+  };
+}
+
+function runAppUsingOptions(container: HTMLElement, data: Options) {
+  /*
+    insert new div at top of sidebar to hold content
+  */
+  const newContainer = document.createElement('div');
+  (container.parentNode as Node).insertBefore(newContainer, container);
+  /*
+    only visually hide sidebar to prevent issues with DOM modification
+  */
+  if (!data.sidebar) {
+    (document.getElementById('right-side') as HTMLElement).className +=
+      'hidden-sidebar';
+  }
+  runApp(newContainer, data);
+}
+
+function runAppInChrome(container: HTMLElement) {
+  chrome.storage.sync.get(storedUserOptions, function (result) {
+    chrome.storage.sync.set(optionsOrDefaults(result as Options), function () {
+      chrome.storage.sync.get(null, function (result2) {
+        runAppUsingOptions(container, result2 as Options);
+      });
+    });
+  });
+}
+
+/* Chrome APIs work fine in firefox currently, but firefox-native implementation is commented here for the future. */
+function runAppInFirefox(container: HTMLElement) {
+  // (async () => {
+  //   // @ts-expect-error: InstallTrigger is only in Firefox
+  //   const options = await browser.storage.sync.get(storedUserOptions);
+  //   // @ts-expect-error: InstallTrigger is only in Firefox
+  //   browser.storage.sync.set(optionsOrDefaults(options as Options));
+  //   // @ts-expect-error: InstallTrigger is only in Firefox
+  //   const updatedOptions = await browser.storage.sync.get(null);
+  //   runAppUsingOptions(container, updatedOptions);
+  // })();
+  runAppInChrome(container);
+}
+
 function createSidebar(
   container: HTMLElement,
   observer?: MutationObserver
 ): void {
   observer?.disconnect();
-  chrome.storage.sync.get(
-    [
-      'startDate',
-      'period',
-      'startHour',
-      'startMinutes',
-      'sidebar',
-      'dash_courses',
-    ],
-    function (result) {
-      chrome.storage.sync.set(
-        {
-          startDate: !result.startDate ? 1 : result.startDate,
-          startHour: !result.startHour ? 15 : result.startHour,
-          startMinutes: !result.startMinutes ? 0 : result.startMinutes,
-          period: !(
-            result.period === 'Day' ||
-            result.period === 'Week' ||
-            result.period === 'Month'
-          )
-            ? 'Week'
-            : result.period,
-          sidebar:
-            result.sidebar !== false && result.sidebar !== true
-              ? false
-              : result.sidebar,
-          dash_courses:
-            result.dash_courses !== false && result.dash_courses !== true
-              ? false
-              : result.dash_courses,
-        },
-        function () {
-          chrome.storage.sync.get(null, function (result2) {
-            const data = result2 as Options;
-            /*
-            insert new div at top of sidebar to hold content
-          */
-            const newContainer = document.createElement('div');
-            (container.parentNode as Node).insertBefore(
-              newContainer,
-              container
-            );
-            /*
-            only visually hide sidebar to prevent issues with DOM modification
-          */
-            if (!result2.sidebar) {
-              (document.getElementById(
-                'right-side'
-              ) as HTMLElement).className += 'hidden-sidebar';
-            }
-            runApp(newContainer, data);
-          });
-        }
-      );
-    }
-  );
+
+  // // @ts-expect-error: InstallTrigger is only in Firefox
+  const isFirefox = false; // typeof InstallTrigger !== 'undefined'
+
+  if (isFirefox) runAppInFirefox(container);
+  else runAppInChrome(container);
 }
 
 if (rightSide) {
