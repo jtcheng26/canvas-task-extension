88a03d4213e1c29bae53d546bdcd182f5cd439fd
jtcheng26 <jtcheng226@gmail.com>
2021-09-14 20:41:16 -0400

    Change due date headings to # of days

--- src/pages/Content/modules/utils/getDueDateHeadingLabel.ts
+++ src/pages/Content/modules/utils/getDueDateHeadingLabel.ts
@@ -1,18 +1,19 @@
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+// const days = [
+//   'Sunday',
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday',
+// ];
 
 export default function getDueDateHeadingLabel(delta: number): string {
   if (delta < 0) return 'Overdue';
   if (delta == 0) return 'Today';
   else if (delta == 1) return 'Tomorrow';
-  else if (delta < 7) return days[(new Date(Date.now()).getDay() + delta) % 7];
+  else if (delta < 7) return `${delta} days`;
+  // days[(new Date(Date.now()).getDay() + delta) % 7];
   else if (delta < 14) return '1 week';
   else return `${Math.floor(delta / 7)} weeks`;
 }
