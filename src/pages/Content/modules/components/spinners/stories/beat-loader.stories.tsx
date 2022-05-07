e665c770b0d9b4d1e1d69d62618a154c45068f17
jtcheng26 <jtcheng226@gmail.com>
2022-05-07 15:48:06 -0400

    Add loader stories

---
+++ src/pages/Content/modules/components/spinners/stories/beat-loader.stories.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import BeatLoader from '../BeatLoader';
+
+export default {
+  title: 'Components/BeatLoader',
+  component: BeatLoader,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof BeatLoader>;
+
+// No Canvas fonts/styling, will look a bit different
+const Template: ComponentStory<typeof BeatLoader> = (args) => (
+  <BeatLoader {...args} />
+);
+
+export const Loader = Template.bind({});
+Loader.args = {
+  color: 'var(--ic-brand-font-color-dark-lightened-30)',
+  loading: true,
+  size: 10,
+};
