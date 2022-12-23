1d39fb34e942e56cc36ff95afc8586aabc057866
Jeffrey Cheng <jtcheng226@gmail.com>
2022-12-23 16:48:35 -0500

    Add build argument to distinguish browsers

--- utils/build.js
+++ utils/build.js
@@ -3,8 +3,12 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
-var webpack = require('webpack'),
-  config = require('../webpack.config');
+const BROWSER_ENV = process.argv[2];
+
+var webpack = require('webpack');
+var config = require('../webpack.config')({
+  browser: BROWSER_ENV,
+});
 
 delete config.chromeExtensionBoilerplate;
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,10 @@ var webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   TerserPlugin = require('terser-webpack-plugin');
 
-const BROWSER_ENV = process.argv[2] || 'chrome';
 const BROWSER_MANIFEST = {
-  chrome: 'src/manifest.json',
+  chrome: 'src/manifest-chrome.json',
   firefox: 'src/manifest-firefox.json',
 };
-
-if (!Object.keys(BROWSER_MANIFEST).includes(BROWSER_ENV)) {
-  console.log('Invalid environment');
-  process.exit();
-}
 
 const ASSET_PATH = process.env.ASSET_PATH || '/';
 
@@ -46,142 +40,153 @@ if (fileSystem.existsSync(secretsPath)) {
   alias['secrets'] = secretsPath;
 }
 
-var options = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    options: path.join(__dirname, 'src', 'pages', 'Options', 'index.ts'),
-    contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.ts'),
-  },
-  chromeExtensionBoilerplate: {
-    notHotReload: ['contentScript'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
-    publicPath: ASSET_PATH,
-  },
-  module: {
-    rules: [
-      {
-        // look for .css or .scss files
-        test: /\.(css)$/,
-        // in the `src` directory
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-      {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-        exclude: /node_modules/,
-      },
-      { test: /\.(ts|tsx)$/, loader: 'ts-loader', exclude: /node_modules/ },
-      {
-        test: /\.(js|jsx)$/,
-        use: [
-          {
-            loader: 'source-map-loader',
-          },
-          {
-            loader: 'babel-loader',
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    alias: alias,
-    extensions: fileExtensions
-      .map((extension) => '.' + extension)
-      .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    // clean the build folder
-    new CleanWebpackPlugin({
-      verbose: true,
-      cleanStaleWebpackAssets: false,
-    }),
-    // expose and write the allowed env vars on the compiled bundle
-    new webpack.EnvironmentPlugin(['NODE_ENV']),
-    new CopyWebpackPlugin({
-      patterns: [
+var options = (args) => {
+  const BROWSER_ENV = args.browser || 'chrome';
+  let opts = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: {
+      options: path.join(__dirname, 'src', 'pages', 'Options', 'index.ts'),
+      contentScript: path.join(
+        __dirname,
+        'src',
+        'pages',
+        'Content',
+        'index.ts'
+      ),
+    },
+    chromeExtensionBoilerplate: {
+      notHotReload: ['contentScript'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].bundle.js',
+      publicPath: ASSET_PATH,
+    },
+    module: {
+      rules: [
         {
-          from: BROWSER_MANIFEST[BROWSER_ENV],
-          to: path.join(__dirname, 'build', 'manifest.json'),
-          force: true,
-          transform: function (content) {
-            // generates the manifest file using the package.json informations
-            return Buffer.from(
-              JSON.stringify({
-                description: process.env.npm_package_description,
-                version: process.env.npm_package_version,
-                ...JSON.parse(content.toString()),
-              })
-            );
+          // look for .css or .scss files
+          test: /\.(css)$/,
+          // in the `src` directory
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+          ],
+        },
+        {
+          test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
           },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+          exclude: /node_modules/,
+        },
+        { test: /\.(ts|tsx)$/, loader: 'ts-loader', exclude: /node_modules/ },
+        {
+          test: /\.(js|jsx)$/,
+          use: [
+            {
+              loader: 'source-map-loader',
+            },
+            {
+              loader: 'babel-loader',
+            },
+          ],
+          exclude: /node_modules/,
         },
       ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/pages/Content/content.styles.css',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets/img',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
-      filename: 'options.html',
-      chunks: ['options'],
-      cache: false,
-    }),
-    new webpack.DefinePlugin({
-      'process.env.DEMO': JSON.stringify(env.DEMO),
-    }),
-    // new BundleAnalyzerPlugin(),
-  ],
-  infrastructureLogging: {
-    level: 'info',
-  },
-};
-
-if (env.NODE_ENV === 'development') {
-  options.devtool = 'cheap-module-source-map';
-} else {
-  options.optimization = {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
+    },
+    resolve: {
+      alias: alias,
+      extensions: fileExtensions
+        .map((extension) => '.' + extension)
+        .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      // clean the build folder
+      new CleanWebpackPlugin({
+        verbose: true,
+        cleanStaleWebpackAssets: false,
       }),
+      // expose and write the allowed env vars on the compiled bundle
+      new webpack.EnvironmentPlugin(['NODE_ENV']),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: BROWSER_MANIFEST[BROWSER_ENV],
+            to: path.join(__dirname, 'build', 'manifest.json'),
+            force: true,
+            transform: function (content) {
+              // generates the manifest file using the package.json informations
+              return Buffer.from(
+                JSON.stringify({
+                  description: process.env.npm_package_description,
+                  version: process.env.npm_package_version,
+                  ...JSON.parse(content.toString()),
+                })
+              );
+            },
+          },
+        ],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/pages/Content/content.styles.css',
+            to: path.join(__dirname, 'build'),
+            force: true,
+          },
+        ],
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/assets/img',
+            to: path.join(__dirname, 'build'),
+            force: true,
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
+        filename: 'options.html',
+        chunks: ['options'],
+        cache: false,
+      }),
+      new webpack.DefinePlugin({
+        'process.env.DEMO': JSON.stringify(env.DEMO),
+      }),
+      // new BundleAnalyzerPlugin(),
     ],
+    infrastructureLogging: {
+      level: 'info',
+    },
   };
-}
+
+  if (env.NODE_ENV === 'development') {
+    opts.devtool = 'cheap-module-source-map';
+  } else {
+    opts.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+        }),
+      ],
+    };
+  }
+
+  return opts;
+};
 
 module.exports = options;
