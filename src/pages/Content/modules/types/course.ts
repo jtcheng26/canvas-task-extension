e5c7a4342636f25675e47b7c2a0d7db6312396e3
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2023-10-01 11:46:01 -0400

    make user selected course names override originals

--- src/pages/Content/modules/types/course.ts
+++ src/pages/Content/modules/types/course.ts
@@ -2,6 +2,7 @@ interface Course {
   id: string;
   color: string; // via dashboard colors request
   name: string; // via course code for now, may change to chosen names
+  original_name?: string; // if the user assigns a new course name, canvas api will add an optional original_name field
   position: number; // via dashboard positions request
   course_code?: string;
   access_restricted_by_date?: boolean;
